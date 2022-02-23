2b920961baf0d4e46f2ca59c2dda23e10fe1a850
Liangbo Jin <jlbPrimer@outlook.com>
2022-02-23 19:28:28 +0800

    Finish projects section

--- server.js
+++ server.js
@@ -13,11 +13,13 @@ app.use(express.static(__dirname + '/public'));
 
 
 // Router Manage
-const indexRouter = require('./routes/index')
+const indexRouter = require('./routes/index');
+const projectRouter = require('./routes/project');
 
-app.use('/',indexRouter)
+app.use('/',indexRouter);
+app.use('/project', projectRouter);
 
 
 app.listen(process.env.PORT || 3000,()=>{
   console.log("Server is up and listening to 3000 now")
-}) 
+});
